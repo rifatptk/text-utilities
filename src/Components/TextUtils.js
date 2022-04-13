92b9998e116e23f236cc2d03e7e33b5ec0feb718
RIfat Shikder <rifatptk@gmail.com>
2022-04-13 12:56:41 +0600

    upper & lower case function added

--- src/Components/TextUtils.js
+++ src/Components/TextUtils.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function TextUtils() {
+  const [text, setText] = useState("");
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  function upperCase() {
+    setText(text.toLocaleUpperCase());
+  }
+  function lowerCase() {
+    setText(text.toLowerCase());
+  }
+
   return (
     <div className="TextUtils">
-        
+      <div className="px-5">
+        <label htmlFor="text-box" className="form-label fw-bold text-warning">
+          Example textarea
+        </label>
+        <textarea
+          onChange={handleOnChange}
+          value={text}
+          className="form-control bg-dark border-warning text-light"
+          id="text-box"
+          rows="5"
+        />
+        <div className="buttons my-2">
+          <button
+            onClick={upperCase}
+            type="button"
+            class="btn btn-outline-warning bg-dark"
+          >
+            Upper Case
+          </button>
+          <button
+            onClick={lowerCase}
+            type="button"
+            class="btn btn-outline-warning bg-dark ms-2"
+          >
+            Lower Case
+          </button>
+          <button type="button" class="btn btn-outline-warning ms-2 bg-dark">
+            Right
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }

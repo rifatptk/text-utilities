7249b60fd5178478540a99f087fd979a73350ec1
RIfat Shikder <rifatptk@gmail.com>
2022-04-13 11:24:20 +0600

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className='header-container my-5 p-5 bg-dark w-25'>
+        <h1 className='fw-bold'>Text<span className='text-warning'>Utilities</span></h1>
+    </div>
+  )
+}
---
+++ src/Components/TextUtils.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TextUtils() {
+  return (
+    <div className="TextUtils">
+        
+    </div>
+  )
+}
